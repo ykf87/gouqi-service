c1f0356afd68b1678becaf901b00497f4ce9f8ef
Gouqi <1603601268@qq.com>
2021-07-14 23:15:12 +0000

    aaa

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::group([
+    'middleware' => 'Jwt',    
+], function(){
+    Route::get('/ss', function(){
+        echo 'sdss';
+    });
 });
