225a290db4b9033d72739c3136cabfe6e506db4f
Your Name <>
2021-07-18 18:34:55 +0800

    11

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -47,6 +47,7 @@ class UserController extends Controller{
 
 		$user 			= User::where(User::$usernameKey, $phone)->first();
 		if($user){
+			dd($user);
 			if(password_verify($pwd, $user->pwd)){
 				return $this->error(__('用户已存在!'));
 			}
