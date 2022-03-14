c6400375848d1969a2305fbea97264ef3b7f85f0
Your Name <you@example.com>
2022-03-14 20:04:19 +0800

    address

--- app/Http/Controllers/Api/Signins/SigninsController.php
+++ app/Http/Controllers/Api/Signins/SigninsController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\Api\Signins;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class SigninsController extends Controller
-{
-    //
+class SigninsController extends Controller{
+	//签到首页
+	public function signed(Request $request){
+		$uid 		= $request->get('_uid');
+	}
 }
