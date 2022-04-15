53ca347f1cfa11b337ab4da898bf60822a6c8293
Your Name <you@example.com>
2022-04-15 21:55:00 +0800

    sign

--- app/Http/Controllers/Api/Signins/SigninsController.php
+++ app/Http/Controllers/Api/Signins/SigninsController.php
@@ -192,8 +192,8 @@ class SigninsController extends Controller{
 						->where("$t2.main_status", 1)
 						->whereRaw("if($t1.start_time > 0, start_time <= $now, true)")
 						->whereRaw("if($t1.end_time > 0, end_time > $now, true)")
-						->inRandomOrder()->limit($limit)->forPage($page)->get();
-						// ->orderByDesc("$t1.sortby")->limit($limit)->forPage($page)->get();
+						// ->inRandomOrder()->limit($limit)->forPage($page)->get();
+						->orderByDesc("$t1.sortby")->limit($limit)->forPage($page)->get();
 		if(count($res) > 0){
 			// foreach($res as &$item){
 		 //        if(substr($item->cover, 0, 4) != 'http'){
