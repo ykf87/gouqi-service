f98b691ac6c871d810ad11bebd292ec51f17a516
Your Name <you@example.com>
2022-04-23 20:57:26 +0800

    choujiang

---
+++ app/Http/Controllers/Api/Sweepstake/SweepstakeController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Api\Sweepstake;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SweepstakeController extends Controller{
+	public function index(Request $request){
+		
+	}
+}
---
+++ app/Models/SweepstakeProduct.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SweepstakeProduct extends Model
+{
+    use HasFactory;
+}
