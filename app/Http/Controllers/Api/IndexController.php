d6a9b5bf512a816051af056b48caacf481385db2
Your Name <you@example.com>
2022-05-10 16:23:28 +0800

    fd

--- app/Http/Controllers/Api/IndexController.php
+++ app/Http/Controllers/Api/IndexController.php
@@ -27,6 +27,11 @@ class IndexController extends Controller{
 		// ];
 		// dd(date('Y-m-d H:i:s', strtotime('+30days')));
 		$arr 		= Config::where('status', 1)->pluck('val', 'key');
+		$arr['loginimg']		= '';
+		$arr['activities'][]	= [
+			'title'		=> '幸运抽奖',
+			'innerimg'	=> ''
+		];
 		return $this->success($arr);
 	}
 
