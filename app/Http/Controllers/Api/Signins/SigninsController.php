50852a0a9f56b420d9f004903d16890af8055102
Your Name <you@example.com>
2022-05-28 16:28:42 +0800

    sfd

--- app/Http/Controllers/Api/Signins/SigninsController.php
+++ app/Http/Controllers/Api/Signins/SigninsController.php
@@ -29,7 +29,8 @@ class SigninsController extends Controller{
 	            if($id > 0){
 	                $user   	= User::select('id', 'name as nickname', 'sex', 'level', 'avatar', 'updated_at')->find($id);
 	                if($user){
-	                	if(strtotime($user->updated_at->format('Y-m-d H:i:s')) > $fenjie){
+	                	$user->updated_at 	= $user->updated_at->format('Y-m-d H:i:s');
+	                	if(strtotime($user->updated_at) > $fenjie){
 	                		try {
 		                        $utime  = $jwt->claims()->get('utime');
 		                        if($utime != $user->updated_at){
@@ -42,6 +43,9 @@ class SigninsController extends Controller{
 	                	$arr['user']	= $user;
 	                	$taskInfos 		= SiginTask::siginInfo($user->id);
 	                	$arr['issigin']	= $taskInfos && isset($taskInfos['issigin']) ? $taskInfos['issigin'] : false;
+	                	if($taskInfos){
+							$arr['signed']	= $taskInfos;
+						}
 	                }
 	            }
 	        }
